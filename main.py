b860c3cf78b8b213ca088b598f080090277851dd
frost-head <ed.ayush2003@gmail.com>
2021-11-12 01:11:09 +0530

    added footer and minors tweaks to home page and it is done

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('Home.html', whatsNew=[{'title':'Check out are new skirts','price':'1299','image':'/static/whatsNew1.jpeg','active':'True'},{'active':'False','title':'Check out are new shirts','price':'1099','image':'/static/whatsNew2.jpeg'}])
+    return render_template('Home.html', whatsNew=[{'title':'Check out are new skirts','price':'1299','image':'/static/whatsNew1.jpeg','active':'True'},{'active':'False','title':'Check out are new shirts','price':'1099','image':'/static/whatsNew2.jpeg'},{'active':'False','title':'Check out are all new math course','price':'599','image':'/static/whatsNew3.jpeg'}])
 
 app.run(debug=True, host= '0.0.0.0')
