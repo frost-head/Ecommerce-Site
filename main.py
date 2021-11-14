1e3730082124ae5cb2061e2a4a36e29f3caa98b0
frost-head <ed.ayush2003@gmail.com>
2021-11-14 10:34:22 +0530

    Added Account Creation Page

--- main.py
+++ main.py
@@ -6,8 +6,17 @@ app = Flask(__name__)
 def home():
     return render_template('Home.html', whatsNew=[{'title':'Check out are new skirts','price':'1299','image':'/static/whatsNew1.jpeg','active':'True'},{'active':'False','title':'Check out are new shirts','price':'1099','image':'/static/whatsNew2.jpeg'},{'active':'False','title':'Check out are all new math course','price':'599','image':'/static/whatsNew3.jpeg'}])
 
+@app.route('/create-account', methods=['GET','POST'])
+def createAccount():
+    return render_template(
+        'CreateAccount.html'
+    )
+
+
 @app.route('/clothing')
 def clothings():
-    return render_template('Clothing.html',results=[{'title':'Check out are new skirts','price':'1299','image':'/static/whatsNew1.jpeg','active':'True'},{'active':'False','title':'Check out are new shirts','price':'1099','image':'/static/whatsNew2.jpeg'},{'title':'Check out are new skirts','price':'1299','image':'/static/whatsNew1.jpeg','active':'True'},{'active':'False','title':'Check out are new shirts','price':'1099','image':'/static/whatsNew2.jpeg'},{'title':'Check out are new skirts','price':'1299','image':'/static/whatsNew1.jpeg','active':'True'},{'active':'False','title':'Check out are new shirts','price':'1099','image':'/static/whatsNew2.jpeg'},{'title':'Check out are new skirts','price':'1299','image':'/static/whatsNew1.jpeg','active':'True'}])
+    return render_template('Clothing.html',results=[{'title':'Check out are new skirts','price':'12','image':'/static/whatsNew1.jpeg','active':'True'},{'title':'Check out are new skirts','price':'12','image':'/static/whatsNew1.jpeg','active':'True'},{'active':'False','title':'Check out are new shirts','price':'1099','image':'/static/whatsNew2.jpeg'},{'title':'Check out are new skirts','price':'1299','image':'/static/whatsNew1.jpeg','active':'True'},{'active':'False','title':'Check out are new shirts','price':'1099','image':'/static/whatsNew2.jpeg'},{'title':'Check out are new skirts','price':'1299','image':'/static/whatsNew1.jpeg','active':'True'},{'active':'False','title':'Check out are new shirts','price':'1099','image':'/static/whatsNew2.jpeg'},{'title':'Check out are new skirts','price':'1299','image':'/static/whatsNew1.jpeg','active':'True'}])
+
+
 
 app.run(debug=True, host= '0.0.0.0')
