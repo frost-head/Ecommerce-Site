b8713871d66e4f95292debe30eca4e13f9d3f027
frost-head <ed.ayush2003@gmail.com>
2021-11-16 16:07:17 +0530

    added basic login register page and cloth description page

--- main.py
+++ main.py
@@ -16,10 +16,15 @@ def createAccount():
 
 @app.route('/clothing')
 def clothings():
-    return render_template('Clothing.html',results=[{'title':'Check out are new skirts','price':'12','image':'/static/whatsNew1.jpeg','active':'True'},{'title':'Check out are new skirts','price':'12','image':'/static/whatsNew1.jpeg','active':'True'},{'active':'False','title':'Check out are new shirts','price':'1099','image':'/static/whatsNew2.jpeg'},{'title':'Check out are new skirts','price':'1299','image':'/static/whatsNew1.jpeg','active':'True'},{'active':'False','title':'Check out are new shirts','price':'1099','image':'/static/whatsNew2.jpeg'},{'title':'Check out are new skirts','price':'1299','image':'/static/whatsNew1.jpeg','active':'True'},{'active':'False','title':'Check out are new shirts','price':'1099','image':'/static/whatsNew2.jpeg'},{'title':'Check out are new skirts','price':'1299','image':'/static/whatsNew1.jpeg','active':'True'}])
+    return render_template('Clothing.html',results=[{'title':'Check out are new skirts','price':'1552','image':'/static/whatsNew1.jpeg','active':'True'},{'title':'Check out are new skirts','price':'12','image':'/static/whatsNew1.jpeg','active':'True'},{'active':'False','title':'Check out are new shirts','price':'1099','image':'/static/whatsNew2.jpeg'},{'title':'Check out are new skirts','price':'1299','image':'/static/whatsNew1.jpeg','active':'True'},{'active':'False','title':'Check out are new shirts','price':'1099','image':'/static/whatsNew2.jpeg'},{'title':'Check out are new skirts','price':'1299','image':'/static/whatsNew1.jpeg','active':'True'},{'active':'False','title':'Check out are new shirts','price':'1099','image':'/static/whatsNew2.jpeg'},{'title':'Check out are new skirts','price':'1299','image':'/static/whatsNew1.jpeg','active':'True'}])
 
+@app.route('/login')
+def login():
+    return render_template('Login.html')
 
-
+@app.route('/cloth')
+def cloth():
+    return render_template('Clothpage.html')
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
